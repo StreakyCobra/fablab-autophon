e0fe99a6defa894a27e241b92c016f7cd0713623
Fabien Dubosson <fabien.dubosson@gmail.com>
2018-03-19 20:35:07 +0100

    WIP

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 """Telegram client for the FabLab Sion."""
 
+################################################################################
+# IMPORTS                                                                      #
+################################################################################
+
 import os
 import threading
+import time
 
 import requests
+import RPi.GPIO as GPIO
 from telethon import TelegramClient, events
+
+################################################################################
+# CONFIGURATIONS                                                               #
+################################################################################
 
 # If an `.env` file is present, parse it contents in the environment variables
 if os.path.exists('.env'):
@@ -23,6 +33,31 @@ EASYDOOR_OPENDOOR = os.environ['EASYDOOR_OPENDOOR']
 EASYDOOR_USERNAME = os.environ['EASYDOOR_USERNAME']
 EASYDOOR_PASSWORD = os.environ['EASYDOOR_PASSWORD']
 
+################################################################################
+# GPIO                                                                         #
+################################################################################
+
+# GPIO mode
+GPIO.setmode(GPIO.BOARD)
+
+# Pins number
+IO_DIAL = 11
+IO_PULSES = 13
+IO_HANGER = 15
+IO_RING = 12
+
+# Inputs
+GPIO.setup(IO_DIAL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)    # Dialing
+GPIO.setup(IO_PULSES, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Dial pulses
+GPIO.setup(IO_HANGER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Hanger
+
+# Outputs
+GPIO.setup(IO_RING, GPIO.OUT)                               #  RING output
+
+################################################################################
+# TELEGRAM                                                                     #
+################################################################################
+
 # Create a telegram client and start it
 CLIENT = TelegramClient('fablab_telegram_client',
                         API_ID,
@@ -31,6 +66,14 @@ CLIENT = TelegramClient('fablab_telegram_client',
                         spawn_read_thread=False)
 CLIENT.start()
 
+@CLIENT.on(events.NewMessage(chats=BOT_ID, incoming=True))
+def on_bot_message(_):
+    """Handle messages from the door bot."""
+    ask_for_door()
+
+################################################################################
+# CLASSES                                                                      #
+################################################################################
 
 class Ring(threading.Thread):
 
@@ -44,16 +87,67 @@ class Ring(threading.Thread):
 
     def run(self):
         """Code of the thread."""
-        self._ring.wait()
-        print('RING')
-        self._ring.clear()
+        while True:
+            self._ring.wait()
+            while self._ring.is_set():
+                GPIO.output(IO_RING, True)
+                time.sleep(.5)
+                GPIO.output(IO_RING, False)
+                time.sleep(1.5)
 
-    def ring(self):
+    def start_ring(self):
         """Ring the phone."""
         self._ring.set()
 
-RING = Ring()
+    def stop_ring(self):
+        """Stop the phone ringing."""
+        self._ring.clear()
 
+class Hanger(threading.Thread):
+
+    """Class handling the hanger of the Autophon."""
+
+    def __init__(self):
+        """Initialize the Ring."""
+        threading.Thread.__init__(self)
+        self._ring = threading.Event()
+        self.start()
+
+    def run(self):
+        """Code of the thread."""
+        while True:
+            self._ring.wait()
+            while self._ring.is_set():
+                GPIO.output(IO_RING, True)
+                time.sleep(.5)
+                GPIO.output(IO_RING, False)
+                time.sleep(1.5)
+
+    def start_ring(self):
+        """Ring the phone."""
+        self._ring.set()
+
+    def stop_ring(self):
+        """Stop the phone ringing."""
+        self._ring.clear()
+
+# Create thread objects
+RING = Ring()
+HANGER = Hanger()
+
+################################################################################
+# FUNCTIONS                                                                    #
+################################################################################
+
+def ask_for_door():
+    """Ask for opening the door through Autophon."""
+    RING.start_ring()
+    channel = GPIO.wait_for_edge(IO_HANGER, GPIO.RISING, timeout=20000)
+    if channel is not None:
+        open_door()
+    else:
+        print(channel)
+    RING.stop_ring()
 
 def open_door():
     """Open the door of the Espace Creation."""
@@ -62,17 +156,20 @@ def open_door():
                                    'login_password': EASYDOOR_PASSWORD})
         return s.get(EASYDOOR_OPENDOOR).status_code == 200
 
-
-@CLIENT.on(events.NewMessage(chats=BOT_ID, incoming=True))
-def on_bot_message(_):
-    """Handle messages from the door bot."""
-    RING.ring()
-
+################################################################################
+# SCRIPT                                                                       #
+################################################################################
 
 def main():
     """Start the Autophon."""
     CLIENT.idle()
 
+def cleanup():
+    """Cleanup the GPIO."""
+    GPIO.cleanup()
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    finally:
+        cleanup()
