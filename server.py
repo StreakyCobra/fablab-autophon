762703e72af7e750a32aced5ac1881115b43b026
Fabien Dubosson <fabien.dubosson@gmail.com>
2018-03-28 16:16:16 +0200

    Improve server.py

--- server.py
+++ server.py
@@ -16,17 +16,17 @@ from logzero import logger
 from telethon import TelegramClient, events
 
 ################################################################################
-# CONFIGURATIONS                                                               #
+# CONFIGURATION                                                                #
 ################################################################################
 
-# If an `.env` file is present, parse it contents in the environment variables
+# If an `.env` file is present, parse its content as environment variables
 if os.path.exists('.env'):
     with open('.env', 'r') as f:
         for line in f.readlines():
             key, val = line.strip().split('=', maxsplit=1)
             os.environ[key.strip()] = val.strip()
 
-# Get secrets
+# Get secrets from environment variables
 API_ID = os.environ['API_ID']
 API_HASH = os.environ['API_HASH']
 BOT_ID = os.environ['BOT_ID']
@@ -35,8 +35,38 @@ EASYDOOR_OPENDOOR = os.environ['EASYDOOR_OPENDOOR']
 EASYDOOR_USERNAME = os.environ['EASYDOOR_USERNAME']
 EASYDOOR_PASSWORD = os.environ['EASYDOOR_PASSWORD']
 
-# Whether a TTY is available
-TTY = sys.stdin.isatty()
+# Check whether it is an interactive session
+IS_INTERACTIVE = sys.stdin.isatty()
+
+# Pin numbers
+IO_DIAL = 11
+IO_RING = 12
+IO_PULSES = 13
+IO_HANGER = 15
+IO_PUSHER = 18
+
+################################################################################
+# EVENTS                                                                       #
+################################################################################
+
+HANGER = threading.Event()
+PUSHER = threading.Event()
+
+def event_handler(channel):
+    """Handle GPIOs events."""
+    logger.debug("Input %s triggered", channel)
+    if channel == IO_HANGER:
+        if not GPIO.input(IO_HANGER):
+            HANGER.set()
+        else:
+            HANGER.clear()
+    elif channel == IO_PUSHER:
+        if GPIO.input(IO_PUSHER):
+            PUSHER.set()
+        else:
+            PUSHER.clear()
+    else:
+        raise ValueError('Event not supported')
 
 ################################################################################
 # GPIO                                                                         #
@@ -44,13 +74,6 @@ TTY = sys.stdin.isatty()
 
 # GPIO mode
 GPIO.setmode(GPIO.BOARD)
-
-# Pins number
-IO_DIAL = 11
-IO_RING = 12
-IO_PULSES = 13
-IO_HANGER = 15
-IO_PUSHER = 18
 
 # Inputs
 GPIO.setup(IO_DIAL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -61,59 +84,48 @@ GPIO.setup(IO_PUSHER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # Outputs
 GPIO.setup(IO_RING, GPIO.OUT)
 
-################################################################################
-# TELEGRAM                                                                     #
-################################################################################
-
-# Create a telegram client and start it
-CLIENT = TelegramClient('fablab_telegram_client',
-                        API_ID,
-                        API_HASH,
-                        update_workers=1,
-                        spawn_read_thread=TTY)
-CLIENT.start()
-
-@CLIENT.on(events.NewMessage(chats=BOT_ID, incoming=True))
-def on_bot_message(_):
-    """Handle messages from the door bot."""
-    ask_for_door()
+# Register GPIOs events detection
+GPIO.add_event_detect(IO_PUSHER,
+                      GPIO.BOTH,
+                      callback=event_handler,
+                      bouncetime=200)
+GPIO.add_event_detect(IO_HANGER,
+                      GPIO.BOTH,
+                      callback=event_handler,
+                      bouncetime=200)
 
 ################################################################################
 # CLASSES                                                                      #
 ################################################################################
 
 class Ring(threading.Thread):
-
     """Class handling the ringing of the Autophon."""
-
     def __init__(self, *args, **kwargs):
-        """Initialize the Ring."""
+        """Initialize the Ring handler."""
         threading.Thread.__init__(self, *args, **kwargs)
-        self._ring = threading.Event()
+        self._trigger = threading.Event()
         self.start()
 
     def run(self):
         """Code of the thread."""
         while True:
-            self._ring.wait()
-            while self._ring.is_set():
+            self._trigger.wait()
+            while self._trigger.is_set():
                 GPIO.output(IO_RING, True)
                 time.sleep(.5)
                 GPIO.output(IO_RING, False)
                 time.sleep(1.5)
 
     def start_ring(self):
-        """Ring the phone."""
-        self._ring.set()
+        """Make the phone ringing."""
+        self._trigger.set()
 
     def stop_ring(self):
         """Stop the phone ringing."""
-        self._ring.clear()
+        self._trigger.clear()
 
 class Door(threading.Thread):
-
     """Class handling the opening of the door."""
-
     def __init__(self, *args, **kwargs):
         """Initialize the Door handler."""
         threading.Thread.__init__(self, *args, **kwargs)
@@ -125,79 +137,106 @@ class Door(threading.Thread):
         while True:
             self._trigger.wait()
             logger.info('Trying to open the door')
-            with requests.Session() as s:
-                s.post(EASYDOOR_LOGINURL, {'login_username': EASYDOOR_USERNAME,
-                                        'login_password': EASYDOOR_PASSWORD})
-                success = s.get(EASYDOOR_OPENDOOR).status_code == 200
+            with requests.Session() as session:
+                session.post(EASYDOOR_LOGINURL,
+                             {'login_username': EASYDOOR_USERNAME,
+                              'login_password': EASYDOOR_PASSWORD})
+                success = session.get(EASYDOOR_OPENDOOR).status_code == 200
                 if success:
                     logger.info('Door opened')
                 else:
                     logger.error('Failed to open the door')
-            self._trigger.clear()
 
     def open(self):
         """Open the door."""
         self._trigger.set()
+        self._trigger.clear()
 
-################################################################################
-# OBJECTS                                                                      #
-################################################################################
+class Direct(threading.Thread):
+    """Class handling the direct request to open the FabLab."""
+    def __init__(self, *args, **kwargs):
+        """Initialize the handler of direct requests."""
+        threading.Thread.__init__(self, *args, **kwargs)
+        self.start()
 
+    def run(self):
+        """Code of the thread."""
+        while True:
+            PUSHER.wait()
+            if REQUEST.is_requesting():
+                REQUEST.cancel()
+            DOOR.open()
+
+class Request(threading.Thread):
+    """Class handling the request to open the FabLab."""
+    def __init__(self, *args, **kwargs):
+        """Initialize the request handler."""
+        threading.Thread.__init__(self, *args, **kwargs)
+        self._trigger = threading.Event()
+        self.start()
+
+    def run(self):
+        """Code of the thread."""
+        while True:
+            self._trigger.wait()
+            timer = threading.Timer(20, self.cancel)
+            RING.start_ring()
+            while self._trigger.is_set():
+                if HANGER.wait(timeout=.5):
+                    logger.info('Someone picked up the phone')
+                    DOOR.open()
+            timer.cancel()
+            RING.stop_ring()
+
+    def request(self):
+        """Request to open the door."""
+        self._trigger.set()
+
+    def is_requesting(self):
+        """Return true if a request is ongoing."""
+        return self._trigger.is_set()
+
+    def cancel(self):
+        """Cancel request to open the door."""
+        self._trigger.clear()
+
+# Create the threads
 RING = Ring(daemon=True)
 DOOR = Door(daemon=True)
-HANGER = threading.Event()
-PUSHER = threading.Event()
+DIRECT = Direct(daemon=True)
+REQUEST = Request(daemon=True)
 
 ################################################################################
-# FUNCTIONS                                                                    #
+# TELEGRAM                                                                     #
 ################################################################################
 
-def input_triggered(channel):
-    """Input triggered."""
-    logger.debug("Input {} triggered".format(channel))
-    if channel == IO_PUSHER:
-        PUSHER.set()
-        PUSHER.clear()
-    elif channel == IO_HANGER:
-        HANGER.set()
-        HANGER.clear()
+# Create a telegram client and start it
+CLIENT = TelegramClient('fablab_telegram_client',
+                        API_ID,
+                        API_HASH,
+                        update_workers=1,
+                        spawn_read_thread=IS_INTERACTIVE)
+CLIENT.start()
 
-def open_door():
-    """Open the door of the Espace Creation."""
-    DOOR.open()
-
-def ask_for_door():
-    """Ask for opening the door through Autophon."""
-    logger.info('Someone is ringing at the door')
-    RING.start_ring()
-    triggered = HANGER.wait(timeout=20)
-    if triggered:
-        logger.info('Someone picked up the phone')
-        open_door()
-    else:
-        logger.warn('Nobody picked up the phone')
-    RING.stop_ring()
+@CLIENT.on(events.NewMessage(chats=BOT_ID, incoming=True))
+def on_bot_message(_):
+    """Handle messages from the door bot."""
+    REQUEST.request()
 
 ################################################################################
 # SCRIPT HANDLING                                                              #
 ################################################################################
 
 def main():
-    """Start the Autophon."""
-    # Register GPIO events detection
-    GPIO.add_event_detect(IO_PUSHER,
-                          GPIO.RISING,
-                          callback=input_triggered,
-                          bouncetime=200)
-    GPIO.add_event_detect(IO_HANGER,
-                          GPIO.FALLING,
-                          callback=input_triggered,
-                          bouncetime=200)
-    if TTY:
-        # Wait for the exit command to stop
+    """Run the Autophon."""
+    # If interactive session, wait for the 'exit' command to stop the server
+    if IS_INTERACTIVE:
+        logger.debug('Interactive session')
         while input() != 'exit':
             pass
+    # Otherwise put the telegram client on idle mode to keep the server running
     else:
+        logger.debug('Non-interactive session')
         CLIENT.idle()
 
 if __name__ == '__main__':
@@ -205,7 +244,7 @@ if __name__ == '__main__':
         logger.info('Starting the autophon')
         main()
     except KeyboardInterrupt:
-        logger.info('Autophon stopped by the user')
+        logger.warning('Autophon stopped by the user')
     finally:
-        logger.info('Clean up the GPIO')
+        logger.debug('Clean up the GPIOs')
         GPIO.cleanup()
